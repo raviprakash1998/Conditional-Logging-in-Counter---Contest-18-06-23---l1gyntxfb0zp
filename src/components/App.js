ae8a5124990f2a8586f89d2f1f25e7ec73abbfa2
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-06-18 13:26:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,18 @@ import '../styles/App.css';
 
 class App extends React.Component{
   
+  constructor(props){
+    super(props)
+    this.state = {count:0}
+  }
+  
   handleClick(){
     this.setState({count: this.state.count + 1}, () => {
       if(this.state.count % 2 == 0) {
         this.forceUpdate();
-        
-    console.log(`Rendering with count:-${this.state.count}`)
-    }
+        console.log(`Rendering with count:-${this.state.count}`)
+      }
     })
-  }
-  constructor(props){
-    super(props)
-    this.state = {count:0}
-    this.handleClick = this.handleClick.bind(this)
   }
 
 
